f02bec8a0b4bbb7e9b5a5e60469295f1d495be55
Ben Barron <forums@benbarron.com>
2011-08-05 13:39:44 +0100

    Possible fix for hibernation.

--- PS3Remote.cs
+++ PS3Remote.cs
@@ -21,6 +21,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Timers;
 
 using HidLibrary;
@@ -236,10 +237,12 @@ namespace PS3BluMote
             try
             {
                 HardwareAPI.DisableDevice(n => n.ToUpperInvariant().Contains
-                    ("_VID&0002054c_PID&0304"), true);
+                    ("_VID&0002054C_PID&0306"), true);
+
+                Thread.Sleep(500);
 
                 HardwareAPI.DisableDevice(n => n.ToUpperInvariant().Contains
-                    ("_VID&0002054c_PID&0304"), false);
+                    ("_VID&0002054C_PID&0306"), false);
             }
             catch
             {
